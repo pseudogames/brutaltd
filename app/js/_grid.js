87a91cf94034a4a04ef87fa70ca5c4dd1c579e4d
Rodrigo Sliachticas <rslferreira@sp.r7.com.br>
2016-05-20 17:30:13 -0300

    Config for babel with webpack to transpile ES2015 to ES5
    New directory structure
    New way to build and start the app

---
+++ app/js/_grid.js
@@ -0,0 +1,7 @@
+export default class Grid {
+	constructor(w,h) {
+		console.log(`New Grid with w:${w}, h:${h}`);
+		this.w = w;
+		this.h = h;
+	}
+}
---
+++ app/js/_vector.js
@@ -0,0 +1,8 @@
+export default class Vector {
+	constructor(x,y,z) {
+		console.log(`New Vector x:${x},y:${y},z:${z}`);
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+}
