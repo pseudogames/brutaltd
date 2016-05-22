4f9357349b552c0b963cdef0b56b9d348bc39b90
Carlo "zED" Caputo <carlo.caputo@gmail.com>
2016-05-22 17:35:51 -0300

    gulp release

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp'),
 	del  = require('del'),
 	exec = require('child_process').exec,
+	scp = require('gulp-scp2'),
 	connect = require('gulp-connect');
 
 gulp.task('connect', function() {
@@ -30,7 +31,7 @@ gulp.task('copy', function () {
 		.pipe(gulp.dest('build'));
 });
 
-gulp.task('watch', ['clean', 'build'], function () {
+gulp.task('watch', ['build'], function () {
 	gulp.watch(['./app/index.html','./app/js/*.js'], ['build']);
 	gulp.watch(['./build/js/*.js'], ['reload']);
 });
@@ -39,6 +40,18 @@ gulp.task('build', ['clean','copy'], function () {
 	exec('node node_modules/webpack/bin/webpack.js', function (err, stdout, stderr) {
 		console.log(stdout);
 		console.log(stderr);
+	});
+});
+
+gulp.task('release', ['build'], function () {
+	gulp.src('build/**')
+	.pipe(scp({
+		host: 'pseudogames.com',
+		username: 'brutaltd',
+		dest: 'web/',
+	}))
+	.on('error', function(err) {
+		console.log(err);
 	});
 });
 
