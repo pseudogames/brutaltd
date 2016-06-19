e848e8a0ee110ae11f0baca4fecd2c21b581f819
Carlo "zED" Caputo <carlo.caputo@gmail.com>
2016-06-19 02:51:22 +0000

    z_offset fix

--- app/js/Ortho.js
+++ app/js/Ortho.js
@@ -11,9 +11,9 @@ export default class Ortho { // convert from 3D to orthogonal 2D
 
 	project(v : Vector, z_offset : number = 0) : Vector {
 		return new Vector(
-			v.x * this.axis.right.x + v.y * this.axis.front.x +  v.z             * this.axis.up.x, // screen x
-			v.x * this.axis.right.y + v.y * this.axis.front.y +  v.z             * this.axis.up.y, // screen y
-			v.x * this.axis.right.z + v.y * this.axis.front.z + (v.z + z_offset) * this.axis.up.z  // z_index
+			v.x * this.axis.right.x + v.y * this.axis.front.x + v.z * this.axis.up.x, // screen x
+			v.x * this.axis.right.y + v.y * this.axis.front.y + v.z * this.axis.up.y, // screen y
+			v.x * this.axis.right.z + v.y * this.axis.front.z + v.z * this.axis.up.z + z_offset  // z_index
 		);
 	}
 
